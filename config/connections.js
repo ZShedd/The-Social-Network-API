a5720545edcdb53713dd8aad7f8891020d42021a
Clown Supreme <zachary.h.shedd@gmail.com>
2023-04-11 14:10:09 -0700

    Attempted fix to run server properly.

--- config/connections.js
+++ config/connections.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialFuseDB';
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/the-social-network-apiDB';
 
 connect(connectionString, {
   useNewUrlParser: true,
